83d7e73864c10ef1a8ad8dfdab66f7d81f185631
Alex Bryner <abryner@doterra.com>
2019-04-04 01:38:52 -0600

    updating for layout under sponsors

--- src/components/app-sponsors/app-sponsors.tsx
+++ src/components/app-sponsors/app-sponsors.tsx
@@ -10,10 +10,22 @@ export class AppSponsors {
   render() {
     return (
       <div class='app-sponsors'>
-        <div class="container">
-          <h1>Sponsors</h1>
-          <p>Coming Back Soon...</p>
-        </div>
+        <cmp-sponsor name="Simplus" level="platinum" url="https://www.simplus.com/" logo="Simplus.png" alt="Simplus" />
+
+        <cmp-sponsor name="Bluewolf" level="gold" url="https://www.bluewolf.com/" />
+        <cmp-sponsor name="Copado" level="gold" url="https://copado.com/" />
+        <cmp-sponsor name="Grow" level="gold" url="https://www.grow.com/" />
+        <cmp-sponsor name="Lucidchart" level="gold" url="https://www.lucidchart.com/" />
+        <cmp-sponsor name="Pluralsight" level="gold" url="https://www.pluralsight.com/" />
+        <cmp-sponsor name="Workato" level="gold" url="https://www.workato.com/" />
+
+        <cmp-sponsor name="Capstorm" level="silver" url="https://www.capstorm.com/" />
+        <cmp-sponsor name="Eide Bailly" level="silver" url="https://technologyconsulting.eidebailly.com/solutions/salesforce/" />
+        <cmp-sponsor name="Financial Force" level="silver" url="https://www.financialforce.com/" />
+        <cmp-sponsor name="GetFeedback" level="silver" url="https://www.getfeedback.com/" />
+        <cmp-sponsor name="Invisible.io" level="silver" url="https://invisible.io/" />
+        <cmp-sponsor name="Prodly" level="silver" url="https://www.prodly.co/" />
+        <cmp-sponsor name="Salesforce Lightning" level="silver" url="https://www.salesforce.com/campaign/lightning/" />
       </div>
     );
   }
