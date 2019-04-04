c2aa1052c3e11df652924689ff362969539ef5af
Alex Bryner <abryner@doterra.com>
2019-04-04 01:36:27 -0600

    upated cmpimage rather than app

--- src/components/cmp-image/cmp-image.spec.ts
+++ src/components/cmp-image/cmp-image.spec.ts
@@ -1,28 +1,28 @@
-import { AppImage } from './cmp-image';
+import { CmpImage } from './cmp-image';
 
 describe('cmp-image', () => {
   it('builds', () => {
-    expect(new AppImage()).toBeTruthy();
+    expect(new CmpImage()).toBeTruthy();
   });
 
   describe('getImage', () => {
     it('returns a blank string if the img is null', () => {
-      const component = new AppImage();
+      const component = new CmpImage();
       expect(component.getImg(undefined)).toEqual('');
     });
 
     it('returns a blank string if the img is null', () => {
-      const component = new AppImage();
+      const component = new CmpImage();
       expect(component.getImg(null)).toEqual('');
     });
 
     it('returns the image with the default image directory', () => {
-      const component = new AppImage();
+      const component = new CmpImage();
       expect(component.getImg('photo.jpg')).toEqual(component.imgDirectory + 'photo.jpg');
     });
 
     it('returns the image with a custom image directory', () => {
-      const component = new AppImage();
+      const component = new CmpImage();
       component.imgDirectory = 'assets/icons/';
       expect(component.getImg('photo.png')).toEqual('assets/icons/photo.png');
     });
