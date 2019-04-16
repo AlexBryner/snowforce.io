253d7e494661b43d060c28d57f8f57552d920a38
Alex Bryner <abryner@doterra.com>
2019-04-15 21:57:09 -0600

    badge icon path update

--- src/components/cmp-image/cmp-image.spec.ts
+++ src/components/cmp-image/cmp-image.spec.ts
@@ -1,30 +1,32 @@
-import { CmpImage } from './cmp-image';
+import { CmpImage } from "./cmp-image";
 
-describe('cmp-image', () => {
-  it('builds', () => {
+describe("cmp-image", () => {
+  it("builds", () => {
     expect(new CmpImage()).toBeTruthy();
   });
 
-  describe('getImage', () => {
-    it('returns a blank string if the img is null', () => {
+  describe("getImage", () => {
+    it("returns a blank string if the img is null", () => {
       const component = new CmpImage();
-      expect(component.getImg(undefined)).toEqual('');
+      expect(component.getImg(undefined)).toEqual("");
     });
 
-    it('returns a blank string if the img is null', () => {
+    it("returns a blank string if the img is null", () => {
       const component = new CmpImage();
-      expect(component.getImg(null)).toEqual('');
+      expect(component.getImg(null)).toEqual("");
     });
 
-    it('returns the image with the default image directory', () => {
+    it("returns the image with the default image directory", () => {
       const component = new CmpImage();
-      expect(component.getImg('photo.jpg')).toEqual(component.imgDirectory + 'photo.jpg');
+      expect(component.getImg("photo.jpg")).toEqual(
+        component.imgDirectory + "photo.jpg"
+      );
     });
 
-    it('returns the image with a custom image directory', () => {
+    it("returns the image with a custom image directory", () => {
       const component = new CmpImage();
-      component.imgDirectory = 'assets/icons/';
-      expect(component.getImg('photo.png')).toEqual('assets/icons/photo.png');
+      component.imgDirectory = "assets/icon/";
+      expect(component.getImg("photo.png")).toEqual("assets/icon/photo.png");
     });
   });
 });
