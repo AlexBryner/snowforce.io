542221fe1f37e46543c4b7bbbe533242147aeac6
Alex Bryner <abryner@doterra.com>
2019-04-26 02:37:42 -0600

    Session Recordings Available

--- src/components/cmp-thank-you/cmp-thank-you.tsx
+++ src/components/cmp-thank-you/cmp-thank-you.tsx
@@ -1,21 +1,24 @@
-import { Component } from '@stencil/core';
+import { Component } from "@stencil/core";
 
 @Component({
-  tag: 'cmp-thank-you',
-  styleUrl: 'cmp-thank-you.scss',
+  tag: "cmp-thank-you",
+  styleUrl: "cmp-thank-you.scss",
   shadow: true
 })
 export class CmpFireworks {
-
   render() {
     return (
       <div class="cmp-thank-you">
         <h1>Thank You To Our Sponsors For A Great Snowforce 2019!</h1>
         <app-sponsors />
-        <h2>Session Recordings Coming Soon On</h2>
+        <h2>Session Recordings Available At</h2>
         <div>
-          <a href="https://www.pluralsight.com/">
-            <img class="pluralsight" src="assets/icon/sponsors/pluralsight.png" alt="Pluralsight" />
+          <a href="https://app.pluralsight.com/search/?q=Snowforce">
+            <img
+              class="pluralsight"
+              src="assets/icon/sponsors/pluralsight.png"
+              alt="Pluralsight"
+            />
           </a>
         </div>
       </div>
